86e979aa9177f2cb179c9904b785bc2396c84df0
destajador <destajador57@hotmail.com>
2018-01-26 12:24:56 -0600

    menu

--- manager/src/app/app.component.ts
+++ manager/src/app/app.component.ts
@@ -6,5 +6,4 @@ import { ApiService } from './api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- manager/src/app/app.module.ts
+++ manager/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -17,11 +22,14 @@ import { UsuarioComponent } from './usuario.component';
 
 import { ApiService } from './api.service';
 import { AuthService } from './auth.service';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CargaComponent } from './carga/carga.component';
 
 const routes = [
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'usuario', component: UsuarioComponent }
+  { path: 'usuario', component: UsuarioComponent },
+  { path: 'carga', component: CargaComponent }
 ];
 
 @NgModule({
@@ -30,17 +38,23 @@ const routes = [
     MensajeComponent,
     RegistroComponent,
     LoginComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    SidebarComponent,
+    CargaComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
     RouterModule.forRoot(routes),
+    LayoutModule,
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
     MatInputModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
     BrowserAnimationsModule
   ],
   providers: [
